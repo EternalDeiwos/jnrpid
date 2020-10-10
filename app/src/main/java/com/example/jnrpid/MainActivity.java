7ecb6b15ef80a614fa66319c1d7d4cd712f86c5f
Greg Linklater <greglink49@gmail.com>
2020-10-10 10:27:30 +0200

    feat(SharedLibraryLoader): utility to load shared libraries

--- app/src/main/java/com/example/jnrpid/MainActivity.java
+++ app/src/main/java/com/example/jnrpid/MainActivity.java
@@ -9,11 +9,16 @@ import jnr.ffi.LibraryLoader;
 import jnr.ffi.types.pid_t;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    SharedLibraryLoader.loadLibrary("jffi-1.2", this.getApplicationContext());
+    LibC lib = LibraryLoader.create(LibC.class).load("c");
+    Log.d(TAG, "result: " + lib.getpid() + "/" + lib.getppid());
   }
 
   public interface LibC {
@@ -22,10 +27,5 @@ public class MainActivity extends AppCompatActivity {
 
     @pid_t
     long getppid();
-  }
-
-  static {
-    LibC lib = LibraryLoader.create(LibC.class).load("c");
-    Log.d("JNR_TEST", "result: " + lib.getpid() + "/" + lib.getppid());
   }
 }
